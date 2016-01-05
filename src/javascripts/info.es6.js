fb64885d37cada6cc8951f0240187d0a7e7ed110
dai <chamestica@gmail.com>
2016-01-06 08:42:08 +0900

    Make the info clear

--- src/javascripts/info.es6.js
+++ src/javascripts/info.es6.js
@@ -12,19 +12,21 @@ const
         'COLD SCENES<br>' +
         '<br>' +
         '<br>' +
-        'H or I: Open/Close this info<br>' +
+        'Press the following keys<br>' +
         '<br>' +
-        '0: Stop<br>' +
-        '1: Random scenes and random frames, under the dark filter <br>' +
-        '2: Go to the next or previous frame, at a regular speed<br>' +
-        '3: 2 + Darken gradually<br>' +
-        '4: Random scenes and random frames, at the max speed<br>' +
-        '5: Go to the next or previous frame, ' +
+        '[H or I] Open/Close this info<br>' +
+        '<br>' +
+        '[0] Stop<br>' +
+        '[1] Random scenes and random frames, under the dark filter <br>' +
+        '[2] Go to the next or previous frame, at a regular speed<br>' +
+        '[3] 2 + Darken gradually<br>' +
+        '[4] Random scenes and random frames, at the max speed<br>' +
+        '[5] Go to the next or previous frame, ' +
           'at the speed according to the volume of the microphone<br>' +
-        '6: Random scenes and Frames in forward order, at the max speed<br>' +
-        '7: Random scenes and Frames in reverse order, at the max speed<br>' +
+        '[6] Random scenes and Frames in forward order, at the max speed<br>' +
+        '[7] Random scenes and Frames in reverse order, at the max speed<br>' +
         '<br>' +
-        'Space: Change the scene<br>' +
+        '[Space] Change the scene<br>' +
         '<br>' +
         '<br>' +
         '<a href="https://youtu.be/DlldYnxNS-k">' +
